1a7d1dbe1b897f3eafe633af6b93d7c140b31433
cheyjax116 <cword1991@gmail.com>
2023-10-20 03:11:32 -0400

    additional updates to README.md file and patching package vuneralbilites in babel/traverse.

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("37ac7f5c07c80d74")
+/******/ 		__webpack_require__.h = () => ("aef8ed53ec0e8daa")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d177f358d846097c"; }
+/******/ 		__webpack_require__.h = function() { return "f669611943da8b53"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
